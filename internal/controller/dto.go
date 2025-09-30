63e1eab4ce92218529127ae6676624a423cc413e
mondy <workplacefocus@proton.me>
2025-09-30 21:58:28 +0300

    add examples

--- internal/controller/dto.go
+++ internal/controller/dto.go
@@ -17,12 +17,12 @@ type getTotalPriceResponseDTO struct {
 }
 
 type getSubscriptionReadDTO struct {
-	ID          string `json:"id"`
-	UserID      string `json:"user_id"`
-	ServiceName string `json:"service_name"`
-	Price       int    `json:"price"`
-	StartDate   string `json:"start_date"`
-	EndDate     string `json:"end_date"`
+	ID          string `json:"id" example:"b6fa4d7c-8f90-4f92-912e-92c644c57a1e"`
+	UserID      string `json:"user_id" example:"a6fa4d7c-8f90-4f92-912e-92c644c57a1e"`
+	ServiceName string `json:"service_name" example:"Yandex Plus"`
+	Price       int    `json:"price" example:"1000"`
+	StartDate   string `json:"start_date" example:"08-2025"`
+	EndDate     string `json:"end_date" example:"09-2025"`
 }
 
 type getSubscriptionsResponseDTO struct {
